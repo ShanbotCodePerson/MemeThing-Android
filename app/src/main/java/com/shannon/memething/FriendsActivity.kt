f70d5ebc2e0785ea33da0392281929f470a9a4b8
Shannon <shannonappdeveloper@gmail.com>
2020-08-04 17:37:03 -0600

    Misc

---
+++ app/src/main/java/com/shannon/memething/FriendsActivity.kt
@@ -0,0 +1,12 @@
+package com.shannon.memething
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FriendsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friends)
+    }
+}

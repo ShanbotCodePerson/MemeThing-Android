2828fd971661e1b44fca4543d13e5d4373b51d35
Shannon <shannonappdeveloper@gmail.com>
2020-08-07 15:05:50 -0600

    Alert Dialogs

---
+++ app/src/main/java/com/shannon/memething/Alerts.kt
@@ -0,0 +1,66 @@
+package com.shannon.memething
+
+import android.R
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.view.LayoutInflater
+
+object Alerts {
+
+    // A basic alert with a message and a dismiss button
+    fun showBasicAlert(title: String, message: String, buttonText: String = "Dismiss", context: Context) {
+        // Create the alert builder
+        val dialogBuilder = AlertDialog.Builder(context)
+
+        // Set up the message and buttons
+        dialogBuilder
+            .setMessage(message)
+            .setCancelable(false)
+            .setNegativeButton(buttonText, DialogInterface.OnClickListener {
+                    dialog, _ -> dialog.cancel()
+            })
+
+        // Create the dialog box
+        val alert = dialogBuilder.create()
+
+        // Set the title of the alert
+        alert.setTitle(title)
+
+        // Show the alert
+        alert.show()
+    }
+
+    // A choice alert with yes and no buttons
+    fun showChoiceAlert(title: String, message: String, yesButtonFunction: (Void) -> Unit,
+                        yesButtonText: String = "Confirm", noButtonText: String = "No", context: Context) {
+        // Create the alert builder
+        val dialogBuilder = AlertDialog.Builder(context)
+
+        // Set up the message and buttons
+        dialogBuilder
+            .setMessage(message)
+            .setCancelable(false)
+            .setPositiveButton(yesButtonText, DialogInterface.OnClickListener { dialog, _ ->
+                dialog.cancel()
+                yesButtonFunction
+            })
+            .setNegativeButton(noButtonText, DialogInterface.OnClickListener { dialog, _ ->
+                dialog.cancel()
+            })
+
+        // Create the dialog box
+        val alert = dialogBuilder.create()
+
+        // Set the title of the alert
+        alert.setTitle(title)
+
+        // Show the alert
+        alert.show()
+    }
+
+    // An alert to show an error
+    fun showErrorAlert() {
+
+    }
+}
--- app/src/main/java/com/shannon/memething/ProfileActivity.kt
+++ app/src/main/java/com/shannon/memething/ProfileActivity.kt
@@ -1,15 +1,23 @@
 package com.shannon.memething
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.util.DisplayMetrics
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.alert_with_text_field.*
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -48,7 +56,11 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     fun resetPasswordButtonTapped(view: View) {
-
+        showTextFieldAlert("Enter Current Password", "First enter your current password",
+            "Enter Password Here", true) {
+            
+            Toast.makeText(applicationContext, "Alert worked and text entered was $it", Toast.LENGTH_SHORT).show()
+        }
     }
 
     fun editScreenNameButtonTapped(view: View) {
@@ -56,7 +68,7 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     fun pointsInfoButtonTapped(view: View) {
-
+        Alerts.showBasicAlert("Points", "Earn points by having your captions chosen in games with your friends", context = this)
     }
 
     fun deleteAccountButtonTapped(view: View) {
@@ -70,5 +82,41 @@ class ProfileActivity : AppCompatActivity() {
         // Return to the login screen
         intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
+    }
+
+    // A text field alert to get input from the user
+    fun showTextFieldAlert(title: String, message: String, hint: String, isPassword: Boolean = false,
+                           confirmButtonText: String = "Confirm", cancelButtonText: String = "Cancel",
+                           confirmButtonFunction: (String) -> Unit) {
+        // Create the alert builder
+        val dialogBuilder = AlertDialog.Builder(this)
+
+        // Add the custom view
+        val li = LayoutInflater.from(this)
+        val textFieldView = li.inflate(R.layout.alert_with_text_field, null)
+        val textField = textFieldView.findViewById<EditText>(R.id.alertTextField)
+        textField.hint = hint
+        if (isPassword) { textField.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD }
+        dialogBuilder.setView(textFieldView)
+
+        // Set up the buttons
+        dialogBuilder
+            .setMessage(message)
+            .setCancelable(false)
+            .setPositiveButton(confirmButtonText, DialogInterface.OnClickListener { dialog, _ ->
+                confirmButtonFunction(textField.text.toString())
+            })
+            .setNegativeButton(cancelButtonText, DialogInterface.OnClickListener { dialog, _ ->
+                dialog.cancel()
+            })
+
+        // Create the dialog box
+        val alert = dialogBuilder.create()
+
+        // Set the title of the alert
+        alert.setTitle(title)
+
+        // Show the alert
+        alert.show()
     }
 }

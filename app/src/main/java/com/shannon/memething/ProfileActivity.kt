6a164e12e4daddebb20a93e8be3d5755a1c73ba7
Shannon <shannonappdeveloper@gmail.com>
2020-08-06 18:30:45 -0600

    Round profile photos

--- app/src/main/java/com/shannon/memething/ProfileActivity.kt
+++ app/src/main/java/com/shannon/memething/ProfileActivity.kt
@@ -2,6 +2,8 @@ package com.shannon.memething
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_profile.*
 
@@ -20,6 +22,11 @@ class ProfileActivity : AppCompatActivity() {
         profileEmailLabel.text = "Email: ${UserController.currentUser?.email}"
         profileScreenNameLabel.text = "Screen Name: ${UserController.currentUser?.screenName}"
         profilePointsLabel.text = "Points: ${UserController.currentUser?.points}"
+
+        // Make the profile photo round
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        profilePhotoCardView.radius = (displayMetrics.widthPixels * 0.6).toFloat()
     }
 
     fun editProfilePhotoButtonTapped(view: View) {
